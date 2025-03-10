35b33b2b9a26eda03e6a97991fe05a6036eecc68
unknown <elotgodsplan2@gmail.com>
2025-03-10 15:15:57 +0100

    Add slug to the views so it can be used dynamically

--- petnet/store/views.py
+++ petnet/store/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from .models import Product
 
 # Create your views here.
-def product_detail(request):
+def product_detail(request, slug):
+    print('Slug:', slug)
     return render(request, 'store/product_detail.html')
