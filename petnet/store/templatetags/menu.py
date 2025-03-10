09c110901e4e3f942b50b5dadedd3ba6c29eb42f
unknown <elotgodsplan2@gmail.com>
2025-03-10 16:08:51 +0100

    Import categories to the menu.py

--- petnet/store/templatetags/menu.py
+++ petnet/store/templatetags/menu.py
@@ -1,7 +1,11 @@
 from django import template
 
+from store.models import Category
+
 register = template.Library()
 
 @register.inclusion_tag('core/menu.html')
 def menu():
-    return {}
+    categories = Category.objects.all()
+
+    return {'categories': categories}
