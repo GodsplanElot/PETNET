6082089910e8441395e5e1c68667d6ef3892eab7
unknown <elotgodsplan2@gmail.com>
2025-03-10 09:28:59 +0100

    Fix bug

--- petnet/store/models.py
+++ petnet/store/models.py
@@ -5,4 +5,9 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50)
-    
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    def __str__(self):
+        return self.title
