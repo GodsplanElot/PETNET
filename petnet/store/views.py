21610d1c2dc508d7ba24ae797797de98d70df990
unknown <elotgodsplan2@gmail.com>
2025-03-10 16:20:40 +0100

    Update storeApp Views to redirect to category details page

--- petnet/store/views.py
+++ petnet/store/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
+def category_detail(request, slug):
+    return render(request, 'store/category_detail.html')
+
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
 
