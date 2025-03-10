d2157338d6153442f2e96db3fe4544203f6a67d1
unknown <elotgodsplan2@gmail.com>
2025-03-10 16:28:37 +0100

    Updeate category detial page with basic html temlplate

--- petnet/store/views.py
+++ petnet/store/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Category, Product
 
 # Create your views here.
 def category_detail(request, slug):
+    category = get_object_or_404(Category)
     return render(request, 'store/category_detail.html')
 
 def product_detail(request, slug):
