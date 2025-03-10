58622f1bc3b5fc97e1bcab8ae7b492e7d1fc3a5a
unknown <elotgodsplan2@gmail.com>
2025-03-10 09:01:03 +0100

    create app to manage products and category of items

--- petnet/userprofile/models.py
+++ petnet/userprofile/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class userprofile(models.Model):
     user = models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
 
-    def _str_(self):
-        return self.user
+    def __str__(self):
+        return self.user.username
