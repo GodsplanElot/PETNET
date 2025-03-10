2cc8f48bedb54cee4dac5e4e7a3328e1f99bf3e3
unknown <elotgodsplan2@gmail.com>
2025-03-10 16:35:27 +0100

    update store views.py

--- petnet/store/views.py
+++ petnet/store/views.py
@@ -3,8 +3,11 @@ from .models import Category, Product
 
 # Create your views here.
 def category_detail(request, slug):
-    category = get_object_or_404(Category)
-    return render(request, 'store/category_detail.html')
+    category = get_object_or_404(Category, slug=slug)
+
+    return render(request, 'store/category_detail.html', {
+        'catergory': category
+    })
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
